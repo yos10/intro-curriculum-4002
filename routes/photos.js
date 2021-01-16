ab293211de411a79e8132078c0ca438c4421dc41
yos10 <yos.ohtake+git@gmail.com>
2021-01-16 14:29:09 +0900

    パスのパラメーターを表示

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,21 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', (req, res, next) => {
   res.send('Some photos');
+});
+
+router.get('/:title', (req, res, next) => {
+  const title = req.params.title;
+  const images = fs.readdirSync('public/images').map(item => path.parse(item).name);
+  if (images.indexOf(title) >= 0) {
+    res.render('photos', { title: title });
+  } else {
+    res.status(404).send('Not Found');
+  }
 });
 
 module.exports = router;
